d2c0dbe1660e832ec0e3226c204bae3a1fa05d90
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-07-07 00:54:25 +0530

    check boolean condition on empty array and object

--- main.js
+++ main.js
@@ -1,24 +1,61 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+console.log('it works');
+function debounce(func, wait) {
+  let timeout;
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      return func.apply(this, args);
+    }, wait);
+  };
+}
 
-setupCounter(document.querySelector('#counter'))
+const debouncFunction = debounce(() => {
+  console.log('debounce');
+}, 5000);
+
+window.addEventListener('resize', debouncFunction);
+
+function hello(params = 'jhon') {
+  // console.log(params);
+  const greet = `${this.name} : ${this.age}`;
+  return greet;
+}
+
+const person = {
+  name: 'John',
+  age: 30,
+};
+
+// direct call and return the output
+const grt = hello.call(person);
+// console.log(grt);
+
+// console.log(hello.apply(person, ['Jhon Deo']));
+
+const sayHello = hello.bind(person);
+
+// const res = sayHello('Jhon Sina');
+
+// console.log(res);
+// Provide some examples of non-boolean value corrcion to a boolean one
+const data = undefined;
+
+function check() {
+  if (!data) {
+    // when value is ''(empty string), 0 , -0, NaN, null, undefined
+    console.log('data is required');
+    return; // stop execution after false
+  }
+  // when value is 'hello', true, [], {}, {a:43}
+  console.log(data);
+}
+check(data);
+
+const hello = () => {
+  console.log('hello');
+};
+
+// ==
+// comment !=
+// &&
