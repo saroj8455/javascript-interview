fc5d6241806691a3974718235d5db2a92cf31ad3
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-07-09 00:10:51 +0530

    impl event deligation with pagination

--- main.js
+++ main.js
@@ -52,10 +52,113 @@ function check() {
 }
 check(data);
 
-const hello = () => {
-  console.log('hello');
-};
-
 // ==
 // comment !=
 // &&
+
+function Person() {
+  this.age = 0;
+
+  // using arrow function
+  // setInterval(() => {
+  //   // console.log(this.age);
+  //   console.log(this);
+  //   // this.age++;
+  // }, 1000);
+  // print global scope of this : window
+  setInterval(function () {
+    /**
+     * log global object window
+     */
+    // console.log(this);
+  }, 1000);
+}
+
+const p = new Person();
+// console.log(p);
+
+/**
+ *
+ * @author saroj8455
+ * make multiple API requests in parallel using promise
+ */
+async function parallelAPICall() {
+  const postAPI = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const commentsAPI = await fetch(
+    'https://jsonplaceholder.typicode.com/comments'
+  );
+
+  // handel error using try catch
+  try {
+    const [postResponse, commentResponse] = await Promise.all([
+      postAPI,
+      commentsAPI,
+    ]);
+    const post = await postResponse.json();
+    const comments = await commentResponse.json();
+    // console.log(post);
+  } catch (error) {
+    console.warn(error);
+  }
+}
+
+parallelAPICall();
+
+async function getPosts() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await response.json();
+  console.log(posts);
+}
+
+async function getComments() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/comments');
+  const comments = await response.json();
+  console.log(comments);
+}
+// getPosts();
+// getComments();
+
+function pagination(pageSize, pageNumber) {
+  const posts = [
+    { id: 1, title: 'post 1' },
+    { id: 2, title: 'post 2' },
+    { id: 3, title: 'post 3' },
+    { id: 4, title: 'post 4' },
+    { id: 5, title: 'post 5' },
+    { id: 6, title: 'post 6' },
+    { id: 7, title: 'post 7' },
+    { id: 8, title: 'post 8' },
+    { id: 9, title: 'post 9' },
+    { id: 10, title: 'post 10' },
+  ];
+  const start = (pageNumber - 1) * pageSize;
+  const end = pageNumber * pageSize;
+  console.log('start', start);
+  console.log('end', end);
+  return posts.slice(start, end);
+
+  // console.log('start', pageSize * pageNumber);
+  // console.log('end', pageSize * pageNumber + pageSize);
+  // const res = posts.slice(2, 4);
+  // console.log(res);
+}
+
+console.log(pagination(2, 1));
+
+// Event Delegation Example
+const parent = document.querySelector('.pagination');
+
+parent.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    handelButton(e.target);
+  }
+});
+
+function handelButton(button) {
+  // extract the textContent
+  const text = button.textContent;
+  console.log('button clicked', text);
+  // get data  pagination method
+  const data = pagination(2, text);
+  console.log(data);
+}
